97b86169f76964973de5b7ace0e185536be5e073
Michael Monson <michaelmonson@gmail.com>
2024-03-31 21:34:47 -0600

    Established Project Structure

---
+++ src/main/java/com/sirorlando/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+   public static void main(String[] args) {
+       System.out.println("Hello, world! This is my text adventure game.");
+       
+       // Add your game logic here
+   }
+}
